48de47aed4768843b62249d4a4c80bf86ba582c8
pjb2370 <dbwhd634@naver.com>
2022-10-31 18:26:37 +0900

    댓글까지 완성

--- src/main/java/com/investmentsite/isc/IscApplication.java
+++ src/main/java/com/investmentsite/isc/IscApplication.java
@@ -1,8 +1,11 @@
 package com.investmentsite.isc;
 
+import com.investmentsite.isc.configurable.RestTemplateClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(RestTemplateClient.class)
 @SpringBootApplication
 public class IscApplication {
 
